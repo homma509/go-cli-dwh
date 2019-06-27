b0ed5bdd55ca55af16c94d53a0cf1963baa9b712
kokihonma <kouki.honma@gmail.com>
2019-06-28 05:39:27 +0900

    Mod create args struct

--- arg.go
+++ arg.go
@@ -4,12 +4,29 @@ import (
 	"flag"
 )
 
-func parseArgs() (string, int, string, string, string) {
+type Args struct {
+	Host     string
+	Port     int
+	Username string
+	Password string
+	Dbname   string
+}
+
+func parseArgs() *Args {
 	host := flag.String("h", "127.0.0.1", "hostname (default=127.0.0.1)")
 	port := flag.Int("p", 5432, "port (default=5432)")
 	username := flag.String("U", "", "username")
 	password := flag.String("W", "", "password")
 	dbname := flag.String("d", "", "dbname")
+
 	flag.Parse()
-	return *host, *port, *username, *password, *dbname
+
+	return &Args{
+		*host,
+		*port,
+		*username,
+		*password,
+		*dbname,
+	}
+
 }
