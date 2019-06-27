e47c230d8db431537edc905fc6489780d3f750cd
kokihonma <kouki.honma@gmail.com>
2019-06-27 23:01:39 +0900

    Mod parse args func

--- arg.go
+++ arg.go
@@ -4,8 +4,12 @@ import (
 	"flag"
 )
 
-func parseArgs(string, string, []string) {
-	host := flag.String("h", "127.0.0.1", "hostname (default=127.0.0.1)"
+func parseArgs() (string, int, string, string, string) {
+	host := flag.String("h", "127.0.0.1", "hostname (default=127.0.0.1)")
+	port := flag.Int("p", 5432, "port (default=5432)")
+	username := flag.String("U", "", "username")
+	password := flag.String("W", "", "password")
+	dbname := flag.String("d", "", "dbname")
 	flag.Parse()
-	return *host, flag.Args(0), flag.Args()[1:]
+	return *host, *port, *username, *password, *dbname
 }
--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	
+	host, port, username, password, dbname := parseArgs()
+	fmt.Println(host, port, username, password, dbname)
 }
